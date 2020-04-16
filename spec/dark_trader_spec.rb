babe1110bb385194f4c3bed46ed60aa9e9564275
cfacco1 <facco.clement@gmail.com>
2020-04-16 19:13:17 +0200

    Test

---
+++ spec/dark_trader_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/dark_trader'
+
+#Test if the program do something and get data 
+
+describe "data data" do
+    it "should return symbol" do
+      expect(crypto_name).not_to be_nil
+      expect(crypto_price).not_to be_nil
+    end
+
+#Test that the program is an array of hashes 
+
+    it "should return a hash" do 
+      expect(solution_big_hash).not_to be_nil
+    end
+end
--- spec/mairie_christmas_spec.rb
+++ spec/mairie_christmas_spec.rb
@@ -1,6 +1,1 @@
-require 'pry'
-require 'open-uri'
-require 'rubygems'
-require 'nokogiri'
-
 require_relative '../lib/mairie_christmas'
